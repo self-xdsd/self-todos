bf66d0e97d5b8b95bc8aa56ce1966d46b0c18ef0
amihaiemil <amihaiemil@gmail.com>
2020-09-01 11:05:54 +0300

    #1 project skeleton

---
+++ src/test/java/com/selfxdsd/todos/SelfTodosApplicationITCase.java
@@ -0,0 +1,21 @@
+package com.selfxdsd.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * SelfPmApplication start-up test.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 0.0.1
+ */
+@SpringBootTest
+class SelfTodosApplicationITCase {
+
+    /**
+     * See if the app starts.
+     */
+    @Test
+    void contextLoads() { }
+
+}
